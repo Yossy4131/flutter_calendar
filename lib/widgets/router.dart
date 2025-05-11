6f43b0e1df9677ae6a2c5aa6b6e86820771cd692
Yossy41 <y.yoshida1031@gmail.com>
2025-05-11 14:13:22 +0900

    add routing file

---
+++ lib/widgets/router.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../home.dart';
+import '../calendar_page.dart';
+
+final router = GoRouter(
+  initialLocation: HomePage.path,
+  routes: [
+    GoRoute(
+      path: HomePage.path,
+      pageBuilder:
+          (BuildContext context, GoRouterState state) =>
+              MaterialPage(child: HomePage()),
+    ),
+    GoRoute(
+      path: CalendarPage.path,
+      pageBuilder:
+          (BuildContext context, GoRouterState state) =>
+              MaterialPage(child: CalendarPage()),
+    ),
+  ],
+);
